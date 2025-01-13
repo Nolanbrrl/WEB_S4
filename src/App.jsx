8a948af1c68e33b3fedab95374207ce491d360b3
Nolan <bourrel.nolan@gmail.com>
2025-01-13 15:45:34 +0100

    deuxième_push

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
 export default function App() {
-  return <main>Hellooooooooooo World</main>
+  return (
+    <div id="character_card">
+      <img id="character_picture" src="" alt="Character" />
+      <div id="character_description">
+        <p className="character_description_text">Nom : </p>
+        <p className="character_description_text">Sexe : </p>
+      </div>
+    </div>
+  );
+  ;
 }
